4c6b3cf7a005be96548d0ac5e7161812a79f6399
libi.chai <chaiguoxing@qq.com>
2019-09-23 13:33:22 +0800

    change vailder translate to zh

--- pkg/net/http/blademaster/binding/default_validator.go
+++ pkg/net/http/blademaster/binding/default_validator.go
@@ -1,15 +1,21 @@
 package binding
 
 import (
+	"fmt"
+	"github.com/pkg/errors"
 	"reflect"
 	"sync"
 
 	"gopkg.in/go-playground/validator.v9"
+	"github.com/go-playground/locales/zh"
+	ut "github.com/go-playground/universal-translator"
+	zh_translations "gopkg.in/go-playground/validator.v9/translations/zh"
 )
 
 type defaultValidator struct {
 	once     sync.Once
 	validate *validator.Validate
+	trans ut.Translator
 }
 
 var _ StructValidator = &defaultValidator{}
@@ -18,7 +24,12 @@ func (v *defaultValidator) ValidateStruct(obj interface{}) error {
 	if kindOfData(obj) == reflect.Struct {
 		v.lazyinit()
 		if err := v.validate.Struct(obj); err != nil {
-			return err
+			errs := err.(validator.ValidationErrors)
+			errStr := "请求参数错误:"
+			for _,v := range errs.Translate(v.trans){
+				errStr += v+";"
+			}
+			return errors.New(errStr)
 		}
 	}
 	return nil
@@ -31,7 +42,14 @@ func (v *defaultValidator) RegisterValidation(key string, fn validator.Func) err
 
 func (v *defaultValidator) lazyinit() {
 	v.once.Do(func() {
+
+		zh := zh.New()
+		uni := ut.New(zh, zh)
+
+		v.trans, _ = uni.GetTranslator("zh")
+
 		v.validate = validator.New()
+		zh_translations.RegisterDefaultTranslations(v.validate, v.trans)
 	})
 }
 
